1dae7dbbdb86d26a64b459ef385e4c9c8b27a1d8
@anna_arutiunian <@anna_arutiunian>
2024-07-25 18:15:23 +0400

    animation lottie player

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import animationData from './AnimationLottie/Animation.json'
 import { Main } from "./Components/Main/Main";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
+            {loading ? (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100vh",
+                    }}
+                >
+                    <Lottie animationData={animationData} />
+                </div>
+            ) : (
+                <Main />
+            )}
             <Main />
         </>
     );
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,27 +6,27 @@ export const NavBar = () => {
         <nav className="nav_bar">
             <ul>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="#" target="_blank" className="underline_anim">
                         Home
                     </a>
                 </li>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="#" target="_blank" className="underline_anim">
                         Remote Jobs
                     </a>
                 </li>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="#" target="_blank" className="underline_anim">
                         Workshops
                     </a>
                 </li>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="#" target="_blank" className="underline_anim">
                         About Us
                     </a>
                 </li>
                 <li>
-                    <a href="#" target="_blank">
+                    <a href="#" target="_blank" className="underline_anim">
                         Contact
                     </a>
                 </li>
